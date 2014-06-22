665d0d3866ad48a136d02d42bb16828944f6869f
udhaya <udhaya.dhanabalan@gmail.com>
2014-06-22 14:19:48 -0700

    added email for check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
 		
 		def fetch_linked_in_details
 				@profile = Linkedin::Profile.get_profile(params[:link_in_url])
-				#~ Emailer.check_email(params[:link_in_url]).deliver
+				Emailer.check_email(params[:link_in_url]).deliver
 		end
 		
 end
