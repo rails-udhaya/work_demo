152a56abf8628c78a5f72ffa50ca0f2cedfa375a
udhaya <udhaya.dhanabalan@gmail.com>
2014-06-22 15:14:51 -0700

    small changes in adding urls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
 		
 		def index
-				@profile = Linkedin::Profile.get_profile("http://www.linkedin.com/in/udhayakumars")
+				#~ @profile = Linkedin::Profile.get_profile("http://www.linkedin.com/in/udhayakumars")
+				@profile = Linkedin::Profile.get_profile("http://www.linkedin.com/pub/shane-rasnak/13/4a4/53")
 		end
 		
 		def fetch_linked_in_details
-				@profile = Linkedin::Profile.get_profile(params[:link_in_url])
+				l_uri = params[:link_in_url].gsub("https","http")
+				@profile = Linkedin::Profile.get_profile(l_uri)
+				#~ @profile = Linkedin::Profile.get_profile(params[:link_in_url])
 				Emailer.check_email(params[:link_in_url]).deliver
 		end
 		
