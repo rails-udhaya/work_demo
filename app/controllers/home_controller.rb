02ad1e55cdf035dfd0ca4f6976c92a541c882d84
udhaya <udhaya.dhanabalan@gmail.com>
2014-07-02 11:44:51 -0700

    changes for mail sending

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,21 +7,21 @@ class HomeController < ApplicationController
 		
 			def linkedin_demo_for_eesamohammed
 				#~ @profile = Linkedin::Profile.get_profile("http://www.linkedin.com/in/udhayakumars")
-				@profile = Linkedin::Profile.get_profile("https://www.linkedin.com/in/eesamohammed")
+				@profile = Linkedin::Profile.get_profile("http://www.linkedin.com/in/eesamohammed")
 		end
 		
 		def fetch_linked_in_details_for_eesa
 				l_uri = params[:link_in_url].gsub("https","http")
 				@profile = Linkedin::Profile.get_profile(l_uri)
 				#~ @profile = Linkedin::Profile.get_profile(params[:link_in_url])
-				Emailer.check_email(params[:link_in_url],"from eesamohammed").deliver
+				Emailer.check_email(params[:link_in_url],"from eesamohammed").deliver if Rails.env == "production"
 		end		
 		
 		def fetch_linked_in_details
 				l_uri = params[:link_in_url].gsub("https","http")
 				@profile = Linkedin::Profile.get_profile(l_uri)
 				#~ @profile = Linkedin::Profile.get_profile(params[:link_in_url])
-				Emailer.check_email(params[:link_in_url],"from rework, mauri").deliver
+				Emailer.check_email(params[:link_in_url],"from rework, mauri").deliver if Rails.env == "production"
 		end
 		
 end
